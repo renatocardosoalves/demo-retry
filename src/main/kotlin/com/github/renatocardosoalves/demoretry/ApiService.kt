fa87ef5096ad2eac814c897882942780eeb02123
Renato Cardoso Alves <rca.develop@gmail.com>
2024-09-04 10:24:49 -0300

    feat: add retry logic to callExternalApi method

--- src/main/kotlin/com/github/renatocardosoalves/demoretry/ApiService.kt
+++ src/main/kotlin/com/github/renatocardosoalves/demoretry/ApiService.kt
@@ -1,6 +1,8 @@
 package com.github.renatocardosoalves.demoretry
 
 import org.slf4j.LoggerFactory
+import org.springframework.retry.annotation.Backoff
+import org.springframework.retry.annotation.Retryable
 import org.springframework.stereotype.Service
 
 @Service
@@ -8,6 +10,11 @@ class ApiService {
 
     private val logger = LoggerFactory.getLogger(ApiService::class.java)
 
+    @Retryable(
+        value = [ApiException::class],
+        maxAttempts = 3,
+        backoff = Backoff(2000)
+    )
     fun callExternalApi() {
         logger.info("Calling external api...")
         throw ApiException("Error calling external api")
